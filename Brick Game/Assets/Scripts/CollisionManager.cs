def8519121ab888074999e37cfdaa63161f8d8c0
jjp1723 <69912002+jjp1723@users.noreply.github.com>
2023-03-07 20:23:22 -0500

    Fixed Errors in CollisionManager

--- Brick Game/Assets/Scripts/CollisionManager.cs
+++ Brick Game/Assets/Scripts/CollisionManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollisionManager : MonoBehaviour
 {
@@ -17,6 +18,8 @@ public class CollisionManager : MonoBehaviour
     public Text thrownText;
     public Text collectibleText;
     public LaunchingBehavior script;
+
+    public Rigidbody2D blockBody;
 
     // Start is called before the first frame update
     void Start()
